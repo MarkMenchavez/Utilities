f68382c15d60153fb50c6f34ec0106348939a7a1
MMENCHA <mark.menchavez@daimler.com>
2021-03-15 21:36:20 +0800

    Build script

---
+++ build.ps1
@@ -0,0 +1,4 @@
+dotnet clean
+dotnet restore -f -v m
+dotnet build --no-restore --nologo -c Release 
+dotnet test --no-build --nologo -v m -c Release /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
